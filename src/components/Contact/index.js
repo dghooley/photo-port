cf72d1c1573737d55747c9d632d2f8c70b48ae11
Derick Hooley <dghooley@gmail.com>
2021-12-16 22:28:23 -0600

    contactFormStructure

---
+++ src/components/Contact/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function ContactForm () {
+
+    return (
+        <section>
+            <h1>Contact me</h1>
+            <form id="contact-form">
+                // name input 
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" />
+                </div>
+                <div>
+                // email input 
+                    <label htmlFor="email">Name:</label>
+                    <input type="email" name="email" />
+                </div>
+                // message text area 
+                <div>
+                    <label htmlFor="message">Name:</label>
+                    <textarea name="message" rows="5" />
+                </div>
+                <button type="submit">Submit</button>
+                </form>
+        </section>
+    )
+}
+
+export default ContactForm;
